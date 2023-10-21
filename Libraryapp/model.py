611d24f8dfb89f0232d6b387d04f8318e1a0234a
ArthuPereira <arthurpdsantoss@gmail.com>
2023-10-21 14:30:27 -0300

    Create the class FuncionarioDAO

--- Libraryapp/model.py
+++ Libraryapp/model.py
@@ -11,3 +11,20 @@ class Funcionario:
     def ola():
         print('ola, eu sou um funcionario')
 
+class FuncionarioDAO:
+    @classmethod
+    def logar(cls, login, senha):
+        finalResult = False
+        with conect() as conexao:
+            with conexao.cursor() as cursor:
+                query = f'SELECT login, senha FROM funcionario WHERE login = "{login}" AND senha = "{senha}"'
+                cursor.execute(query)
+                result = cursor.fetchall()
+
+                while result:
+                    infoLogin= result[0]['login']
+                    infoSenha= result[0]['senha']
+                    a = Funcionario()
+                    result.pop()
+                    finalResult = True
+        return finalResult
